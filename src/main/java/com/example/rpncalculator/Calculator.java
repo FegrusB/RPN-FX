e44ee10541bd5d390558e6c8f009a580ee3887b3
FegrusB <fergus@beckerleg.co.uk>
2026-03-13 16:40:14 +0000

    basic shunting algorithm

--- src/main/java/com/example/rpncalculator/Calculator.java
+++ src/main/java/com/example/rpncalculator/Calculator.java
@@ -1,5 +1,9 @@
 package com.example.rpncalculator;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Calculator {
@@ -25,7 +29,32 @@ public class Calculator {
 
     }
 
+    public Integer calculateShunting(String input) {
 
+        String[] inputSplit = input.split(" ");
 
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Stack<String> operatorStack = new Stack<>();
+
+        for (String s : inputSplit) {
+            if (StringUtils.isNumeric(s)) {
+                queue.add(Integer.parseInt(s));
+            } else {
+                operatorStack.push(s);
+            }
+        }
+
+        return calculate(recombine(queue, operatorStack));
+    }
+
+    private String recombine( Queue<Integer> queue, Stack<String> operatorStack){
+
+        StringBuilder postfix = new StringBuilder(String.valueOf(queue.poll()));
+
+        for (Integer i : queue) { postfix.append(" ").append(i);}
+        for (String s : operatorStack) { postfix.append(" ").append(s);}
+
+        return postfix.toString();
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.rpncalculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.lang3;
 
 
     opens com.example.rpncalculator to javafx.fxml;
