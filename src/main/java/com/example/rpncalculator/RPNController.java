86cf8f32d73852839f753e2e15930ea63dc02e63
FegrusB <fergus@beckerleg.co.uk>
2026-03-13 15:36:43 +0000

    add clear and equals, clear warnings

--- src/main/java/com/example/rpncalculator/RPNController.java
+++ src/main/java/com/example/rpncalculator/RPNController.java
@@ -3,10 +3,8 @@ package com.example.rpncalculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 public class RPNController {
 
@@ -21,11 +19,17 @@ public class RPNController {
         // Find and configure all buttons within buttonGroup
         for (javafx.scene.Node node : buttonGroup.getChildren()) {
             if (node instanceof Button button) {
-                button.setOnAction(event -> {
-                    input.appendText(button.getText());
-                });
+                if (button.getText().equals("=")) { button.setOnAction(this::calculate);
+                } else if (button.getText().equals("Clear")) {button.setOnAction(this::clear);
+                } else {
+                    button.setOnAction(event -> input.appendText(button.getText()));
+                }
             }
         }
     }
+
+    public void calculate(ActionEvent event) {}
+
+    public void clear(ActionEvent event) {input.clear();}
 
 }
